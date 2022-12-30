8ae6b7634d126c168d25fac7f758ea0b2ebe667b
CMay-30 <179757442@qq.com>
2022-12-31 00:33:51 +0800

    rename

--- laboratory_backend/main.go
+++ laboratory_backend/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"log"
 	"math/rand"
 	"net/http"
@@ -12,17 +11,19 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.GET("/api/auth/register", func(c *gin.Context) {
+	r.POST("/api/auth/register", func(c *gin.Context) {
 		// 获取参数
-		name := context.PostForm("name")
-		telephone := context.PostForm("telephone")
-		password := context.PostForm("password")
+		name := c.PostForm("name")
+		telephone := c.PostForm("telephone")
+		password := c.PostForm("password")
 		// 数据验证
 		if len(telephone) != 11 {
-			context.JSON(http.StatusUnprocessableEntity, map[string]interface{}{"code": 422, "msg": "手机号必须为11位"})
+			c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号必须为11位"})
+			return
 		}
-		if len(password) != 6 {
-			context.JSON(http.StatusUnprocessableEntity, map[string]interface{}{"code": 422, "msg": "密码不能少于6位"})
+		if len(password) < 6 {
+			c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码不能少于6位"})
+			return
 		}
 
 		// 如果名称没有传，给一个10位的随机字符串
@@ -36,19 +37,21 @@ func main() {
 		// 创建用户
 
 		// 返回结果
-		context.JSON(200, gin.H{
+		c.JSON(200, gin.H{
 			"msg": "注册成功",
 		})
 	})
 	panic(r.Run()) // 监听并在 0.0.0.0:8080 上启动服务
 }
 
-func RandomString(n int) {
-	var letters = []byte("aasdfghjklqwertyuiopzxcvbnmASDFGHJKLQWERTYUIOPZXCVBNM")
+// 随机指定位数的字符串
+func RandomString(n int) string {
+	var letters = []byte("asdfghjklqwertyuiopzxcvbnmASDFGHJKLQWERTYUIOPZXCVBNM")
 	result := make([]byte, n)
 
 	rand.Seed(time.Now().Unix())
 	for i := range result {
 		result[i] = letters[rand.Intn(len(letters))]
 	}
+	return string(result)
 }
